96161b2b9afc2a4229e56cfa64899b62a22d041c
Ramanoreet kaur <ramanpreetkaurmalhi474@gmail.com>
2026-06-21 14:52:52 -0700

    server working + Hello message recicved

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # NetConnect Server
 import socket
-server =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 55555))
+server =socket.socket(socket.AF_INET, socket.sock_STREAM)
+server.bind(("127.0.0.1",55555))
 server.listen()
 print("Server is listening...")
 
@@ -9,3 +9,4 @@ connection, address = server.accept()
 print("Client connected")
 message = connection.recv(1024).decode()
 print(message)
+
