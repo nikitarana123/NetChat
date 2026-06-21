abee67b033fc8ff840af8104356039ad5a3bfe7b
nikitarana123 <nikitarana3838@gmail.com>
2026-06-22 00:07:51 +0200

    Updated client and documentation

--- client.py
+++ client.py
@@ -2,5 +2,21 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 55555))
-client.send("Hello" .encode())
-print("Connected to server")
+
+username=input("Enter username:")
+client.send(username.encode())
+
+while True:
+    message =input("Enter your message: ")
+
+    if message.upper() == "EXIT":
+        print("Exiting...")
+        break
+
+    client.send(message .encode())
+
+client.close()
+print("Disconnected")
+
+    
+
