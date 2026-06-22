2170bde444933760bf4db7616e4a4ace8117127d
nikitarana123 <nikitarana3838@gmail.com>
2026-06-22 20:04:36 +0200

    Completed client receive and multi-client testing

--- client.py
+++ client.py
@@ -1,13 +1,27 @@
 # NetConnect Client
 import socket
+import threading
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 55555))
 
 username=input("Enter username:")
 client.send(username.encode())
 
+def receive_messages():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print(message)
+        except:
+            break
+
+thread = threading.Thread(target=receive_messages)
+thread.daemon = True
+thread.start()
+
 while True:
-    message =input("Enter your message: ")
+    message = input("Enter your message: ")
 
     if message.upper() == "EXIT":
         print("Exiting...")
@@ -17,6 +31,8 @@ while True:
 
 client.close()
 print("Disconnected")
+
+
 
     
 
--- server.py
+++ server.py
@@ -4,8 +4,7 @@ import threading
 
 clients = []
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+server =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("127.0.0.1", 55555))
 
 server.listen()
