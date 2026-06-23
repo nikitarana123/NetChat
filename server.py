61cbd58f2ce6d5b61264890c6a6ecca34102363c
Ramanoreet kaur <ramanpreetkaurmalhi474@gmail.com>
2026-06-22 17:24:05 -0700

    Added room creation and join room

--- server.py
+++ server.py
@@ -3,7 +3,13 @@ import socket
 import threading
 
 clients = []
+usernames = {}
 
+rooms = {
+    "General": []
+}
+user_rooms = {}
+    
 server =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("127.0.0.1", 55555))
 
@@ -19,7 +25,8 @@ def handle_client(connection, address):
     username = connection.recv(1024).decode()
 
     print("Username:", username)
-
+    usernames[connection] = username    
+    user_rooms[connection] = "General"
     connection.send("Welcome!".encode())
 
     while True:
@@ -31,7 +38,58 @@ def handle_client(connection, address):
                 break
 
             print(username + ": " + message)
+            if message == "ROOMS":
+               
+               room_list = ", ".join(rooms.keys())
 
+               connection.send(
+                  ("Available rooms: " + room_list).encode()
+               )
+
+               continue
+            if message.startswith("CREATE_ROOM|"):
+
+              room_name = message.split("|")[1]
+
+              if room_name not in rooms:
+                 rooms[room_name] = []
+
+                 connection.send(
+                  ("Room created: " + room_name).encode()
+                )
+
+              else:
+                 connection.send(
+                    ("Room already exists").encode()
+                )
+
+              continue
+            if message.startswith("JOIN_ROOM|"):
+
+               room_name = message.split("|")[1]
+
+               if room_name in rooms:
+
+                 old_room = user_rooms[connection]
+
+                 if connection in rooms[old_room]:
+                       rooms[old_room].remove(connection)
+
+                 rooms[room_name].append(connection)
+
+                 user_rooms[connection] = room_name
+
+                 connection.send(
+                    ("Joined room: " + room_name).encode()
+                 )
+
+                else:
+
+                     connection.send(
+                         ("Room does not exist").encode()
+                     )
+                continue
+            
             #Broadcasting
             for client in clients:
                  if client != connection:
@@ -56,6 +114,8 @@ while True:
     print("NEW CONNECTION:", address)
 
     clients.append(connection)
+
+    rooms["General"].append(connection)
 
     thread = threading.Thread(
         target=handle_client,
